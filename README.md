0c079e3ca84c65f9056aac048887d0713ec2bc9c
theNonDangerousButInvisibleAndAnonymousCoder <thecat@thedog.themouse>
2023-11-26 19:56:05 -0300

    hello

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 
 - bienvenue a metis fm tous les hits
 - vas au README dans cado pour personnaliser des affiches pour la pae d'accueil [lien par ici](./cado/)
+- precise ta variable d'environment MAMUSIQUE pour ou est ta musique qui doit etre jouee (le lieu "relatif" de ta musique) (par ex. "../mymusic/")
 ---
 * pour lancer  le serveur
 ```
--- son.py
+++ son.py
@@ -1,9 +1,11 @@
 from myfunc import Myfunc
 from fichier import Fichier
+import os
 class Son(Myfunc):
     def __init__(self,name):
         self.name=name
-        self.content=Fichier("../radiohaker/public/uploads",name.split("/")[-1]).lirefichier()
+        self.content=Fichier(os.environ("MAMUSIQUE"),name.split("/")[-1]).lirefichier()
+
         self.mymusic=name.split(".")[1]
 
     def get_html(self):
